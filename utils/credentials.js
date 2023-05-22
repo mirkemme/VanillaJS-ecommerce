35c4a5840fb4e92a34417f06e130c6f51bffadb5
Mirko <131855881+mirkemme@users.noreply.github.com>
2023-05-22 10:06:01 +0200

    first commit

---
+++ utils/credentials.js
@@ -0,0 +1,17 @@
+export const usersDB = [
+    {
+        id: 1,
+        username: "pippo",
+        password: "qwerty",
+    },
+    {
+        id: 2,
+        username: "paperino",
+        password: "qwerty",
+    },
+    {
+        id: 3,
+        username: "pluto",
+        password: "qwerty",
+    },
+];
